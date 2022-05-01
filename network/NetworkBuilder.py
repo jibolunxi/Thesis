1070550d3e95a8af2549b585d7a7e544587baf39
changjie <2193927906@qq.com>
2022-05-01 19:23:25 +0800

    构建fork网络&打标签

--- network/NetworkBuilder.py
+++ network/NetworkBuilder.py
@@ -1,7 +1,37 @@
+import csv
 
-SOURCE_DATA_DIRECTORY = 'G:\\thesis2022\\'
+SOURCE_DATA_DIRECTORY = 'G:\\thesis2022\\second_filter\\'
 TARGET_DIRECTORY = 'G:\\networks\\'
 
 
 def fork_network():
-    print(0)
+    links_file = TARGET_DIRECTORY + 'fork_links.csv'
+    links_write = open(links_file, 'a', encoding='utf-8', newline='')
+    links_writer = csv.writer(links_write)
+    links_writer.writerow(['Source', 'Target', 'Weight', 'Type'])
+    nodes_file = TARGET_DIRECTORY + 'fork_nodes.csv'
+    nodes_write = open(nodes_file, 'a', encoding='utf-8', newline='')
+    nodes_writer = csv.writer(nodes_write)
+    nodes_writer.writerow(['Id', 'Label'])
+    nodes_id = set()
+
+    pros_name = {}
+    pro_file = SOURCE_DATA_DIRECTORY + 'new_projects.csv'
+    pro_read = open(pro_file, 'r', encoding='utf-8')
+    pro_reader = csv.reader(pro_read)
+    for pro in pro_reader:
+        pros_name[pro[0]] = pro[1][29:]
+
+    pro_read = open(pro_file, 'r', encoding='utf-8')
+    pro_reader = csv.reader(pro_read)
+    for pro in pro_reader:
+        if pro[-4] != '\\N' and pro[-4] in pros_name:
+            nodes_id.add(pro[0])
+            nodes_writer.writerow([pro[0], pro[1][29:]])
+            links_writer.writerow([pro[0], pro[-4], 1, 'undirected'])
+            if pro[-4] not in nodes_id:
+                nodes_id.add(pro[-4])
+                nodes_writer.writerow([pro[-4], pros_name[pro[-4]]])
+
+
+
---
+++ network/main.py
@@ -0,0 +1,14 @@
+from network import fork_network
+
+
+def network_build():
+    fork_network()
+
+
+def main():
+    # 网络构建
+    network_build()
+    
+
+if __name__ == '__main__':
+    main()
